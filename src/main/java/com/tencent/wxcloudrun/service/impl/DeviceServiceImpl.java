c697985b867d2d0e5e5f2caf902144dffa9b0cf4
aling <aling1472@hotmail.com>
2022-07-25 22:06:38 +0800

    add service annotation

--- src/main/java/com/tencent/wxcloudrun/service/impl/DeviceServiceImpl.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/DeviceServiceImpl.java
@@ -4,9 +4,11 @@ import com.tencent.wxcloudrun.dao.DeviceMapper;
 import com.tencent.wxcloudrun.model.Device;
 import com.tencent.wxcloudrun.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class DeviceServiceImpl implements DeviceService {
     @Autowired
     DeviceMapper deviceMapper;
