a8b3da043caae0332c7e4d2e83ce7297516b561d
aling <aling1472@hotmail.com>
2022-08-15 23:19:05 +0800

    add Component

--- src/main/java/com/tencent/wxcloudrun/config/ApplicationProperties.java
+++ src/main/java/com/tencent/wxcloudrun/config/ApplicationProperties.java
@@ -2,9 +2,11 @@ package com.tencent.wxcloudrun.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "application.config")
 @Data
+@Component
 public class ApplicationProperties {
     public String authorization;
 
